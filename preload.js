57d3d9eec9a353a646ead92ad68cbe3213c3b4f9
Cortez Felipe <felipecortez.fgc@gmail.com>
2020-02-22 14:58:31 -0300

    inicio projeto

---
+++ preload.js
@@ -0,0 +1,19 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+var path = require('path');
+var userName = process.env['USERPROFILE'].split(path.sep)[2];
+var loginId = path.join("domainName",userName);
+console.log(userName);
+//document.write(userName)
+})
+
+
